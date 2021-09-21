a7a949cd47e171192001ffc0752c883718e5a95b
introvertCode <stanislaw.bielski.programista@gmail.com>
2021-09-21 21:04:08 +0200

    removed-pass

--- App/Mail.php
+++ App/Mail.php
@@ -36,7 +36,7 @@ class Mail
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'stanislaw.bielski.programista@gmail.com';                     //SMTP username
-            $mail->Password   = 'nE%F&6OfEy2e';                               //SMTP password
+            $mail->Password   = 'secret';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS` 465
 
